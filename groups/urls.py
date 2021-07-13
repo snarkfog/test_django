15fd24e0dc3a4b29b858e39f10ae1d46fd14ce12
Snarkfog <snarkfog@gmail.com>
2021-07-13 17:00:30 +0300

    Lesson 12 homework complete.

--- groups/urls.py
+++ groups/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from groups.views import create_group, get_groups, update_group
+from groups.views import create_group, delete_group, get_groups, update_group
 
 app_name = 'groups'
 
 urlpatterns = [
     path('', get_groups, name='list'),
     path('create/', create_group, name='create'),
-    path('update/<int:id>', update_group, name='update'),
+    path('update/<int:id>/', update_group, name='update'),
+    path('delete/<int:pk>/', delete_group, name='delete'),
 ]
