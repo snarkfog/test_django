2ed18fab3b608e793f6d182ca733f03d74537ce1
Snarkfog <snarkfog@gmail.com>
2021-07-13 15:00:27 +0300

    Lesson 12 materials complete.

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from students.views import create_student, generate_students, get_students, update_student
+from students.views import create_student, delete_student, generate_students, get_students, update_student
 
 app_name = 'students'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('generate_students/', generate_students),
     path('', get_students, name='list'),
     path('create/', create_student, name='create'),
-    path('update/<int:id>', update_student, name='update'),
+    path('update/<int:id>/', update_student, name='update'),
+    path('delete/<int:pk>/', delete_student, name='delete'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 from faker import Faker
@@ -88,7 +88,7 @@ def create_student(request):
 # @csrf_exempt
 def update_student(request, id): # noqa
 
-    student = Student.objects.get(id=id)
+    student = get_object_or_404(Student, id=id)
 
     if request.method == 'GET':
 
@@ -110,5 +110,21 @@ def update_student(request, id): # noqa
         template_name='students/update.html',
         context={
             'form': form
+        }
+    )
+
+
+def delete_student(request, pk):
+    student = get_object_or_404(Student, id=pk)
+
+    if request.method == 'POST':
+        student.delete()
+        return HttpResponseRedirect(reverse('students:list'))
+
+    return render(
+        request=request,
+        template_name='students/delete.html',
+        context={
+            'student': student
         }
     )
