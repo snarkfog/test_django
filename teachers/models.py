640ac3412c74ea1c78caee02e236e79356d6e427
Snarkfog <snarkfog@gmail.com>
2021-07-25 16:15:34 +0300

    Lesson 13 homework complete.

--- teachers/models.py
+++ teachers/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 from faker import Faker
 
+from groups.models import Group
+
 
 # Create your models here.
 
@@ -19,10 +21,16 @@ class Teacher(models.Model):
     phone_number = models.CharField(max_length=15, unique=True, null=True)
     email = models.EmailField(max_length=120, null=True)
     experience = models.IntegerField(default=5, null=False)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='teachers')
 
 # Homework 8
     def __str__(self):
-        return f'{self.first_name}, {self.last_name }, {self.phone_number}, {self.email}, {self.experience}'
+        return f'{self.first_name}, ' \
+               f'{self.last_name }, ' \
+               f'{self.group}, ' \
+               f'{self.phone_number}, ' \
+               f'{self.email}, ' \
+               f'{self.experience}'
 
     @staticmethod
     def generate_teachers(count):
--- teachers/views.py
+++ teachers/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from teachers.forms import TeacherCreateForm, TeacherUpdateForm
+from teachers.forms import TeacherCreateForm, TeacherUpdateForm, TeachersFilter
 from teachers.models import Teacher
 
 from webargs import fields
@@ -35,11 +35,14 @@ def get_teachers(request, args):
         if param_value:
             teachers = teachers.filter(**{param_name: param_value})
 
+    obj_filter = TeachersFilter(data=request.GET, queryset=teachers)
+
     return render(
         request=request,
         template_name='teachers/list.html',
         context={
-            'teachers': teachers
+            'teachers': teachers,
+            'obj_filter': obj_filter,
         }
     )
 
